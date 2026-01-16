4e654f8a8f35a48d92da56f1cd62daf864474b11
DEVSAM1966 <desarrollo.devsam@gmail.com>
2026-01-16 20:14:41 +0100

    Implementar nuevos dataholders Author

--- src/main/java/com/codigojava/biblioteca/dataholders/AuthorsUpdatedDh.java
+++ src/main/java/com/codigojava/biblioteca/dataholders/AuthorsUpdatedDh.java
@@ -2,21 +2,13 @@ package com.codigojava.biblioteca.dataholders;
 
 import jakarta.validation.constraints.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
-public class AuthorsUpdatedDh {
+@EqualsAndHashCode(callSuper = true)
+public class AuthorsUpdatedDh extends AuthorsBaseDh {
 
     @NotNull(message = "The authorId cannot be null")
     @Min(value = 1, message = "The authorId must be greater than 0")
     private Integer authorId;
-
-    @NotBlank(message = "The author name cannot be empty")
-    @Size(min = 4, max = 100,
-            message = "The author name must be between 4 and 100 characters")
-    @Pattern(
-            regexp = "^[\\p{L} .'’\\-]+$",
-            message = "The author name can only contain letters, spaces, dots, hyphens and apostrophes"
-    )
-    private String nameAuthor;
-
 }
