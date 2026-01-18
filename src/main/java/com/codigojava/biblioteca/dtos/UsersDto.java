b10c90da8a890c56e608639ed3c0926b85544321
DEVSAM1966 <desarrollo.devsam@gmail.com>
2026-01-18 20:43:05 +0100

    Añadir dto Users

---
+++ src/main/java/com/codigojava/biblioteca/dtos/UsersDto.java
@@ -0,0 +1,40 @@
+package com.codigojava.biblioteca.dtos;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class UsersDto {
+
+   private Integer userId;
+
+   private String fullname;
+
+   private String dni;
+
+   private String address;
+
+   private String city;
+
+   private String province;
+
+   private String postalCode;
+
+   private String country;
+
+   private String phone;
+
+   private String email;
+
+   private String password;
+
+   private Date registrationDate;
+
+   private boolean userDrop;
+
+   private Integer daysDisciplinary;
+   
+   private String role;
+
+}
