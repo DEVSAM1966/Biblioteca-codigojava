1b7937ec144ab428ddab2d2e1a328341388fe0b1
DEVSAM1966 <desarrollo.devsam@gmail.com>
2026-01-23 22:06:53 +0100

    Añadir validaciones users

--- src/main/java/com/codigojava/biblioteca/controllers/UsersController.java
+++ src/main/java/com/codigojava/biblioteca/controllers/UsersController.java
@@ -5,6 +5,7 @@ import com.codigojava.biblioteca.services.UsersService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.codigojava.biblioteca.exceptions.DhValidationException;
+import com.codigojava.biblioteca.dataholders.NameValidationGenericDh;
+import com.codigojava.biblioteca.validators.DhValidator;
 
 import java.util.List;
 
@@ -24,6 +28,9 @@ public class UsersController {
     @NonNull
     private UsersService usersService;
 
+    @Autowired
+    private DhValidator dhValidator;
+
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<UsersDto>> findAll() {
         return ResponseEntity.ok(this.usersService.findAll());
@@ -31,11 +38,22 @@ public class UsersController {
 
     @GetMapping(value = "/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UsersDto> findById(@Validated @PathVariable Integer id) {
+
+        if (id == null || id <= 0) {
+            throw new DhValidationException("id", "The id must be a positive integer greater than 0");
+        }
+
         return ResponseEntity.ok(this.usersService.findById(id));
     }
 
     @GetMapping(value = "/name/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<UsersDto>> findByName(@Validated @PathVariable String name) {
+
+        NameValidationGenericDh dh = new NameValidationGenericDh();
+        dh.setName(name);
+
+        dhValidator.validate(dh);
+
         return ResponseEntity.ok(this.usersService.findByName(name));
     }
 
